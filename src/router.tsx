8320702ceb955cd1df5af76325e27389d1c098b3
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-11-26 15:12:33 +0100

    initial commit

---
+++ src/router.tsx
@@ -0,0 +1,55 @@
+import { ConvexQueryClient } from "@convex-dev/react-query";
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter } from "@tanstack/react-router";
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
+import { routerWithQueryClient } from "@tanstack/react-router-with-query";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { DefaultCatchBoundary } from "./components/DefaultCatchBoundary";
+import { NotFound } from "./components/NotFound";
+import { routeTree } from "./routeTree.gen";
+
+export function getRouter() {
+	const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL!;
+	if (!CONVEX_URL) {
+		console.error("missing envar VITE_CONVEX_URL");
+	}
+
+	const convex = new ConvexReactClient(CONVEX_URL, {
+		unsavedChangesWarning: false,
+	});
+	const convexQueryClient = new ConvexQueryClient(convex);
+
+	const queryClient: QueryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				queryKeyHashFn: convexQueryClient.hashFn(),
+				queryFn: convexQueryClient.queryFn(),
+			},
+		},
+	});
+	convexQueryClient.connect(queryClient);
+
+	const router = routerWithQueryClient(
+		createRouter({
+			routeTree,
+			defaultPreload: "intent",
+			defaultErrorComponent: DefaultCatchBoundary,
+			defaultNotFoundComponent: () => <NotFound />,
+			scrollRestoration: true,
+			context: { queryClient, convexClient: convex, convexQueryClient },
+			Wrap: ({ children }: { children: React.ReactNode }) => (
+				<ConvexProvider client={convexQueryClient.convexClient}>
+					{children}
+				</ConvexProvider>
+			),
+		}),
+		queryClient
+	);
+
+	setupRouterSsrQueryIntegration({
+		router,
+		queryClient,
+	});
+
+	return router;
+}
